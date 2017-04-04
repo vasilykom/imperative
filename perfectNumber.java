3e659d06e686f6c8e9356837aae92add9a7d0d5f
Vasily <vasilykom@gmail.com>
2017-04-04 17:34:20 +0300

    Updated
    Streams are in.

--- perfectNumber.java
+++ perfectNumber.java
@@ -8,46 +8,34 @@ import java.util.stream.Stream;
 
 public class perfectNumber {
 
-	public enum STATE { 
-		DEFICIENT, PERFECT, ABUNDANT;
-	}
+    public enum STATE {
+        DEFICIENT, PERFECT, ABUNDANT;
+    }
 
-	public static Set<Integer> divisors(int n) { 
-		Set<Integer> result = new HashSet<>();
+    public static Set<Integer> divisors(int n) {
+        Set<Integer> set = IntStream
+                .rangeClosed(1, n)
+                .filter((i) -> n % i == 0)
+                .boxed()
+                .collect(Collectors.toSet());
+        return set;
+    }
 
-//		Set<Integer> streaFromItereate = Stream.iterate(1, x -> x +1);
-//		Set<Integer> set = IntStream.range(1, n)
-//				.boxed()
-//				.filter(n)
-//				.collect(Collectors.toSet());
-//		return result;
-//	}
 
-		for (int i = 1; i <= n; i++) {
-			if (n % i == 0) { 
-				result.add(i);
-			}
-		}
-		
-		return result;
-	}
+    public static STATE process(int n) {
+        int sum = divisors(n)
+                .stream()
+                .filter((i) -> i != n)
+                .reduce((summa, i) -> summa + i)
+                .orElse(0);
 
-	
-	public static STATE process(int n) {
-		int sum = 0; 
-		
-		for (int i: divisors(n)) { 
-			if (i != n) 
-				sum += i;
-		}
-		
-		if (sum < n) { 
-			return STATE.DEFICIENT; 
-		} else if (sum == n) { 
-			return STATE.PERFECT;
-		} else { 
-			return STATE.ABUNDANT;
-		}
-	}
+        if (sum < n) {
+            return STATE.DEFICIENT;
+        } else if (sum == n) {
+            return STATE.PERFECT;
+        } else {
+            return STATE.ABUNDANT;
+        }
+    }
 
 }
